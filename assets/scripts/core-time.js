26083040a2c4509fe96f9917367a4feb7b2db206
ixwrites <74097341+ixwrites@users.noreply.github.com>
2022-06-04 20:12:37 +0800

    Update core-time.js

--- assets/scripts/core-time.js
+++ assets/scripts/core-time.js
@@ -9,16 +9,15 @@
  */
 
 const gameMinutesUI = document.querySelector('#game-minutes-ui');
-const savedHoursUI = document.querySelector('#game-hours-ui');
+const gameHoursUI = document.querySelector('#game-hours-ui');
 const gameDaysUI = document.querySelector('#game-days-ui');
 const pausePlayBtn = document.querySelector('#pause-play-btn');
 
 let gameSpeed = 625;
-let gameMinutes = 0;
+let gameStatus = 'paused';
+let savedMinutes = parseInt(localStorage.getItem('elapsed_game_minutes'));
 let savedHours = parseInt(localStorage.getItem('elapsed_game_hours'));
 let savedDays = parseInt(localStorage.getItem('elapsed_game_days'));
-
-let gameStatus = 'paused';
 
 window.addEventListener('load', (event) => {
   startTimer();
@@ -26,7 +25,7 @@ window.addEventListener('load', (event) => {
 
 function startTimer() {
   timerInterval = setInterval(function () {
-    gameMinutes++;
+    localStorage.setItem('elapsed_game_minutes', savedMinutes++);
     gameStatus = 'running'
     initGameBackground();
     initDays();
@@ -47,36 +46,51 @@ function initDays() {
 function initHours() {
   if (localStorage.getItem('elapsed_game_hours') === null) {
     localStorage.setItem('elapsed_game_hours', 0);
-    savedHoursUI.innerText = `${savedHours}`;
+    gameHoursUI.innerText = `${savedHours}`;
   } else {
-    savedHoursUI.innerText = `${savedHours}`;
+    if (savedHours < 10) {
+      gameHoursUI.innerText = `0${savedHours}`;
+    } else {
+      gameHoursUI.innerText = `${savedHours}`;
+    }
   }
-
-  // Pad 0 for minutes if less than 10
-  if (savedHours < 10) {
-    savedHoursUI.innerText = `0${savedHours}`;
-  } else {
-    savedHoursUI.innerText = `${savedHours}`;
-  }
-
-  // Reset Day Cycle / Increment Day + 1
+  // Reset Day Cycle
   if (savedHours > 23) {
     savedHours = 0;
+    // Increment Day + 1
     localStorage.setItem('elapsed_game_days', savedDays += 1);
     gameDaysUI.innerHTML = `Day ${savedDays}<br>`;
   }
 }
 
 function initMinutes() {
-  if (gameMinutes > 59) {
-    gameMinutes = 0;
+  if (localStorage.getItem('elapsed_game_minutes') === null) {
+    localStorage.setItem('elapsed_game_minutes', 0);
+    gameMinutesUI.innerText = `${savedMinutes}`;
+  } else {
+    if (savedMinutes < 10) {
+      gameMinutesUI.innerText = `0${savedMinutes}`;
+    } else {
+      gameMinutesUI.innerText = `${savedMinutes}`;
+    }
+  }
+
+
+
+
+
+
+
+
+  if (savedMinutes > 59) {
+    savedMinutes = 0;
     gameMinutesUI.innerText = `00`;
     localStorage.setItem('elapsed_game_hours', savedHours += 1);
   } else {
-    if (gameMinutes < 10) {
-      gameMinutesUI.innerText = `0${gameMinutes}`;
+    if (savedMinutes < 10) {
+      gameMinutesUI.innerText = `0${savedMinutes}`;
     } else {
-      gameMinutesUI.innerText = `${gameMinutes}`;
+      gameMinutesUI.innerText = `${savedMinutes}`;
     }
   }
 }
@@ -123,18 +137,18 @@ function initGameBackground() {
   if (savedHours == 0 || savedHours == 1 || savedHours == 2 || savedHours == 24) {
     document.body.style.backgroundImage = `url('assets/backgrounds/7.png')`;
   } else if (savedHours == 3 || savedHours == 4 || savedHours == 5) {
-    document.body.style.backgroundImage = "url('assets/backgrounds/8.png')";
+    document.body.style.backgroundImage = `url('assets/backgrounds/8.png')`;
   } else if (savedHours == 6 || savedHours == 7 || savedHours == 8) {
-    document.body.style.backgroundImage = "url('assets/backgrounds/1.png')";
+    document.body.style.backgroundImage = `url('assets/backgrounds/1.png')`;
   } else if (savedHours == 9 || savedHours == 10 || savedHours == 11) {
-    document.body.style.backgroundImage = "url('assets/backgrounds/2.png')";
+    document.body.style.backgroundImage = `url('assets/backgrounds/2.png')`;
   } else if (savedHours == 12 || savedHours == 13 || savedHours == 14) {
-    document.body.style.backgroundImage = "url('assets/backgrounds/3.png')";
+    document.body.style.backgroundImage = `url('assets/backgrounds/3.png')`;
   } else if (savedHours == 15 || savedHours == 16 || savedHours == 17) {
-    document.body.style.backgroundImage = "url('assets/backgrounds/4.png')";
+    document.body.style.backgroundImage = `url('assets/backgrounds/4.png')`;
   } else if (savedHours == 18 || savedHours == 19 || savedHours == 20) {
-    document.body.style.backgroundImage = "url('assets/backgrounds/5.png')";
+    document.body.style.backgroundImage = `url('assets/backgrounds/5.png')`;
   } else if (savedHours == 21 || savedHours == 22 || savedHours == 23) {
-    document.body.style.backgroundImage = "url('assets/backgrounds/6.png')";
+    document.body.style.backgroundImage = `url('assets/backgrounds/6.png')`;
   }
 }
